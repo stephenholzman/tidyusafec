35d882087c6d658b68026b6f609e8eac70ef2dc9
StephenHolzman <stephen@holzmansmith.com>
2018-04-22 20:05:02 -0400

    Search Multiple Candidate Totals
    Returns tibbles now and supports multiple candidate IDs at once.

--- R/candidate.R
+++ R/candidate.R
@@ -14,12 +14,12 @@
 #' @param designation The one-letter designation code of the organization: - A: authorized by a candidate - J: joint fundraising committee - P: principal campaign committee of a candidate - U: unauthorized - B: lobbyist/registrant PAC - D: leadership PAC
 #' @param type The one-letter type code of the organization:  - C: communication cost - D: delegate - E: electioneering communication - H: House - I: independent expenditor (person or group) - N: PAC - nonqualified - O: independent expenditure-only (super PACs) - P: presidential - Q: PAC - qualified - S: Senate - U: single candidate independent expenditure - V: PAC with non-contribution account, nonqualified - W: PAC with non-contribution account, qualified - X: party, nonqualified - Y: party, qualified - Z: national party non-federal account
 #' @param full_election Get totals for full election period. Boolean.
-#' @param candidate_id A character string of the OpenFEC endpoint.
+#' @param candidate_ids A character vector of candidate ids to get financial totals for. Required to run.
 #' @param api_key API key for https://api.data.gov. Get one at https://api.data.gov/signup.
 
-get_candidate_totals <- function(data_structure = 'both', candidate_id = NULL,
+get_candidate_totals <- function(data_structure = 'both', candidate_ids = NULL,
                               sort_null_only = NULL, cycle = NULL, sort = NULL, designation = NULL, type = NULL,
-                              full_election = NULL, api_key = NULL){
+                              full_election = TRUE, api_key = NULL){
 
   if (is.null(api_key)) {
 
@@ -27,76 +27,101 @@ get_candidate_totals <- function(data_structure = 'both', candidate_id = NULL,
 
   }
 
-  query_parameters <- list(
-    sort_null_only = sort_null_only, cycle = cycle, sort = sort, designation = designation, type = type,
-    full_election = full_election, api_key = api_key
-  )
+  if (is.null(candidate_ids)) {
 
-  query_parameters <- query_parameters[!sapply(query_parameters, is.null)]
-
-  responses <- list()
-
-  responses[[1]] <- get_openfec(path = paste0("/candidate/",candidate_id, "/totals/"), query = query_parameters)
-
-  total_pages <- responses[[1]][["pagination"]][["pages"]]
-
-  total_count <- responses[[1]][["pagination"]][["count"]]
-
-  if(total_count > 100){
+    stop('Argument for candidate_ids is required')
 
   }
 
-  if(total_pages > 1){
+  responses <- list()
 
-    for(i in 2:total_pages){
+  for(c_id in candidate_ids){
+    query_parameters <- list(
+      sort_null_only = sort_null_only, cycle = cycle, sort = sort, designation = designation, type = type,
+      full_election = full_election, api_key = api_key, page = 1, per_page = 100
+    )
 
-      query_parameters$page <- i
+    query_parameters <- query_parameters[!sapply(query_parameters, is.null)]
 
-      responses[[i]] <- get_openfec(path = "/candidates/search/", query = query_parameters)
-      print(i)
+    responses[[c_id]][[1]] <- get_openfec(path = paste0("/candidate/",c_id, "/totals/"), query = query_parameters)
+
+    total_pages <- responses[[c_id]][[1]][["pagination"]][["pages"]]
+
+    total_count <- responses[[c_id]][[1]][["pagination"]][["count"]]
+
+    if(total_count > 100){
+
+    }
+
+    if(total_pages > 1){
+
+      for(i in 2:total_pages){
+
+        query_parameters$page <- i
+
+        responses[[c_id]][[i]] <- get_openfec(path = paste0("/candidate/",c_id, "/totals/"), query = query_parameters)
+        print(i)
+
+      }
 
     }
 
   }
 
-  # tidy_candidates <- purrr::map(responses, function(x) x$results) %>%
-  #   unlist(recursive = F) %>%
-  #   tibble(
-  #     load_date = map_chr(. , "load_date", .default = NA),
-  #     last_file_date = map_chr(. , "last_file_date", .default = NA),
-  #     candidate_id = map_chr(. , "candidate_id", .default = NA),
-  #     election_years = map(. , "election_years", .default = NA),
-  #     party = map_chr(. , "party", .default = NA),
-  #     candidate_status = map_chr(. , "candidate_status", .default = NA),
-  #     office_full = map_chr(. , "office_full", .default = NA),
-  #     state = map_chr(. , "state", .default = NA),
-  #     federal_funds_flag = map_lgl(. , "federal_funds_flag", .default = NA),
-  #     has_raised_funds = map_lgl(. , "has_raised_funds", .default = NA),
-  #     office = map_chr(. , "office", .default = NA),
-  #     election_districts = map(. , "election_districts", .default = NA),
-  #     name = map_chr(., "name", .default = NA),
-  #     principal_committees = map(. , "principal_committees"),
-  #     district_number = map_int(. , "district_number", .default = NA),
-  #     cycles = map(. , "cycles", .default = NA),
-  #     incumbent_challenge = map_chr(. , "incumbent_challenge", .default = NA),
-  #     party_full = map_chr(. , "party_full", .default = NA),
-  #     first_file_date = map_chr(. , "first_file_date", .default = NA),
-  #     active_through = map_int(. , "active_through", .default = NA),
-  #     incumbent_challenge_full = map_chr(. , "incumbent_challenge_full", .default = NA),
-  #     last_f2_date = map_chr(. , "last_f2_date", .default = NA),
-  #     district = map_chr(. , "district", .default = NA)
-  #   ) %>%
-  #   tidyr::replace_na(list(principal_committees = list(list(list(organization_type_full = NA, designation = NA, state = NA,
-  #                                                                cycles = NA, party_full = NA, committee_type_full = NA,
-  #                                                                organization_type = NA, committee_id = NA, first_file_date = NA,
-  #                                                                party = NA, committee_type = NA, last_file_date = NA, candidate_ids = NA,
-  #                                                                designation_full = NA, last_f1_date = NA, treasurer_name = NA, name = NA))))) %>%
-  #   tidyr::unnest(principal_committees, .preserve = c("election_years", "cycles", "election_districts")) %>%
-  #   mutate(committee_id = map(principal_committees, `[`, "committee_id") %>% unlist() %>% as.vector(),
-  #          committee_name = map(principal_committees, `[`, "name") %>% unlist() %>% as.vector())
-  #
+  tidy_candidates <- responses %>%
+    unlist(recursive = F) %>%
+    purrr::map(function(x) x$results) %>%
+    unname() %>%
+    unlist(recursive = F) %>%
+    tibble(
+      loan_repayments = map_dbl(. , "loan_repayments", .default = NA),
+      candidate_id = map_chr(. , "candidate_id", .default = NA),
+      last_net_operating_expenditures = map_dbl(. , "last_net_operating_expenditures", .default = NA),
+      transfers_from_other_authorized_committee = map_dbl(. , "transfers_from_other_authorized_committee", .default = NA),
+      receipts = map_dbl(. , "receipts", .default = NA),
+      offsets_to_fundraising_expenditures = map_dbl(. , "offsets_to_fundraising_expenditures", .default = NA),
+      other_receipts = map_dbl(. , "other_receipts", .default = NA),
+      candidate_contribution = map_dbl(. , "candidate_contribution", .default = NA),
+      individual_unitemized_contributions =map_dbl(. , "individual_unitemized_contributions", .default = NA),
+      operating_expenditures = map_dbl(. , "operating_expenditures", .default = NA),
+      individual_contributions = map_dbl(. , "individual_contributions", .default = NA),
+      fundraising_disbursements = map_dbl(. , "fundraising_disbursements", .default = NA),
+      political_party_committee_contributions = map_dbl(. , "political_party_committee_contributions", .default = NA),
+      coverage_start_date = map_chr(. , "coverage_start_date", .default = NA),
+      exempt_legal_accounting_disbursement = map_dbl(. , "exempt_legal_accounting_disbursement", .default = NA),
+      full_election = map_lgl(. , "full_election", .default = NA),
+      loan_repayments_candidate_loans = map_dbl(. , "loan_repayments_candidate_loans", .default = NA),
+      offsets_to_legal_accounting = map_dbl(. , "offsets_to_legal_accounting", .default = NA),
+      last_debts_owed_to_committee = map_dbl(. , "last_debts_owed_to_committee", .default = NA),
+      transfers_to_other_authorized_committee = map_dbl(. , "transfers_to_other_authorized_committee", .default = NA),
+      refunded_other_political_committee_contributions = map_dbl(. , "refunded_other_political_committee_contributions", .default = NA),
+      last_debts_owed_by_committee = map_dbl(. , "last_debts_owed_to_committee", .default = NA),
+      net_contributions = map_dbl(. , "net_contributions", .default = NA),
+      refunded_political_party_committee_contributions = map_dbl(. , "refunded_political_party_committee_contributions", .default = NA),
+      federal_funds = map_dbl(. , "federal_funds", .default = NA),
+      other_disbursements = map_dbl(. , "other_disbursements", .default = NA),
+      coverage_end_date = map_chr(. , "coverage_end_date", .default = NA),
+      last_net_contributions = map_dbl(. , "last_net_contributions", .default = NA),
+      offsets_to_operating_expenditures = map_dbl(. , "offsets_to_operating_expenditures", .default = NA),
+      last_report_year = map_int(. , "last_report_year", .default = NA),
+      other_political_committee_contributions = map_dbl(. , "other_political_committee_contributions", .default = NA),
+      individual_itemized_contributions = map_dbl(. , "individual_itemized_contributions", .default = NA),
+      loans = map_dbl(. , "loans", .default = NA),
+      cycle = map_int(. , "cycle", .default = NA),
+      last_report_type_full = map_chr(. , "last_report_type_full", .default = NA),
+      last_cash_on_hand_end_period = map_dbl(. , "last_cash_on_hand_end_period", .default = NA),
+      loan_repayments_other_loans = map_dbl(. , "loan_repayments_other_loans", .default = NA),
+      loans_made_by_candidate = map_dbl(. , "loans_made_by_candidate", .default = NA),
+      net_operating_expenditures = map_dbl(. , "net_operating_expenditures", .default = NA),
+      contributions = map_dbl(. , "contributions", .default = NA),
+      contribution_refunds = map_dbl(. , "contribution_refunds", .default = NA),
+      total_offsets_to_operating_expenditures = map_dbl(. , "total_offsets_to_operating_expenditures", .default = NA),
+      disbursements = map_dbl(. , "disbursements", .default = NA),
+      last_beginning_image_number = map_chr(. , "last_beginning_image_number", .default = NA),
+      all_other_loans = map_dbl(. , "all_other_loans", .default = NA)
+    )
   object_to_return <- list(
-    #tidy = tidy_candidates,
+    tidy = tidy_candidates,
     raw_responses = responses
   )
 
