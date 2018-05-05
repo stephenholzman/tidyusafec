01ecbb1d793250085ffc54b68643dfbd1e494b5f
StephenHolzman <stephen@holzmansmith.com>
2018-05-05 17:37:46 -0400

    Cleanup endpoint based file names
    Moving to one function per file.

--- R/helpers.R
+++ R/helpers.R
@@ -1,5 +1,17 @@
 #Note: The code for data_gov_api_key is lifted from Kyle Walker's tidycensus package (MIT License).
 
+#' Save a data.gov API key
+#'
+#' It's best practice not to include your API key in scripts. Save it interactively with this function, then it will be available with Sys.getenv("DATAGOV_API_KEY"). All 'api_key' arguments in tidyfec functions default to Sys.getenv("DATAGOV_API_KEY").
+#'
+#' @param key
+#' @param overwrite
+#' @param install
+#'
+#' @return
+#' @export
+#'
+#' @examples
 data_gov_api_key <- function (key, overwrite = FALSE, install = FALSE)
 {
   if (install) {
